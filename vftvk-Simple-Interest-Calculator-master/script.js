aa80e80be2869f4320f43ead4a6dd05060dc536d
Onel-R <leonreuben@yahoo.com>
2025-04-28 15:42:28 -0400

    Update project files

---
+++ vftvk-Simple-Interest-Calculator-master/script.js
@@ -0,0 +1,29 @@
+function compute()
+{
+    let p = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+
+    if (p <= 0) {
+        alert("Please enter a positive number!");
+        document.getElementById("principal").focus();
+    } else {
+        let interest = p * years * (rate/100);
+        var amount = parseInt(p) + parseFloat(interest);
+        var year = new Date().getFullYear() + parseInt(years);
+
+        let result = document.getElementById("result");
+        result.innerText = `If you deposit <mark> ${p} </mark>,\<br\>` +
+                        `at an interest rate of \<mark\> ${rate}\% \</mark\>.\<br\>` +
+                        `You will receive an amount of \<mark\> ${interest} \</mark\>,\<br\>` +
+                        `in the year \<mark\> ${year} \</mark\> \<br\>`;
+
+    }
+}
+
+function updateRate() {
+    let rate = document.getElementById("rate").value;
+    let rateVal = document.getElementById("rate_val");
+    rateVal.innerText = rate;
+}
+        

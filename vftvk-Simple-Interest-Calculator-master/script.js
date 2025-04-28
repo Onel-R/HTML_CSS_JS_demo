169521e41c9923a80136819ffdd8d419872edaea
Onel-R <leonreuben@yahoo.com>
2025-04-28 16:04:28 -0400

    Update js file

--- vftvk-Simple-Interest-Calculator-master/script.js
+++ vftvk-Simple-Interest-Calculator-master/script.js
@@ -13,9 +13,9 @@ function compute()
         var year = new Date().getFullYear() + parseInt(years);
 
         let result = document.getElementById("result");
-        result.innerHTML = `If you deposit <mark> ${p} </mark>,<br>` +
+        result.innerHTML = `If you deposit <mark> $${p} </mark>,<br>` +
                         `at an interest rate of <mark> ${rate}% </mark>.<br>` +
-                        `You will receive an amount of <mark> ${interest} </mark>,<br>` +
+                        `You will receive an amount of <mark>  $${amount} </mark>,<br>` +
                         `in the year <mark> ${year} </mark> <br>`;
 
     }
